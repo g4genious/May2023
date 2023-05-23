553196b52579a1a46fd62ea1630c0354dbb26a31
g4genious <g_4_genious@yahoo.com>
2023-05-23 18:57:00 +0500

    Add project files.

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace May2023.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        
+    }
+}
